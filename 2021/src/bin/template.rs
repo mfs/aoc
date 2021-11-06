3a3ed6e33d060caedbdcf0e5998d826b702929c2
Mike Sampson <mike@sambodata.com>
2021-11-06 21:43:04 +1100

    Initial commit

---
+++ 2021/src/bin/template.rs
@@ -0,0 +1,48 @@
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    println!("Advent of Code 2021!");
+
+    /* process stdin as lines
+    use std::io::{self, BufRead};
+    for line in io::stdin().lock().lines() {
+        println!("--- {}", line?);
+    }
+    */
+
+    /* process stdin as chars (ascii only)
+    use std::io::{self, Read};
+    for byte in io::stdin().lock().bytes() {
+        println!("{}", byte? as char);
+    }
+    */
+
+    /* process stdin as lines of space separated ints
+    use std::io::{self, BufRead};
+    for line in io::stdin().lock().lines() {
+        let x: Vec<u64> = line?
+            .split(' ')
+            .map(|x| x.parse())
+            .collect::<Result<_, _>>()?;
+        println!("{:?}", x);
+    }
+    */
+
+    /* process stdin using split and regex
+    use std::io::{self, Read};
+    use regex::Regex;
+    let mut s = String::new();
+    io::stdin().lock().read_to_string(&mut s)?;
+
+    let re = Regex::new(r"([LR])(\d+)")?;
+
+    for e in s.split(",") {
+        if let Some(caps) = re.captures(e) {
+            let r = &caps[1];
+            let d: i64 = caps[2].parse()?;
+        }
+    }
+    */
+
+    Ok(())
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2018 - 2021
+
+- [2018](https://github.com/mfs/aoc-2018)
+- [2020](https://github.com/mfs/aoc-2020)
+- 2021 (This repo)
+
