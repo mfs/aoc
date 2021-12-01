e8b7812b804844d72ad02b2592a3cd6600b4f548
Mike Sampson <mike@sambodata.com>
2021-12-01 21:47:50 +1100

    cargo-fmt cleanup

--- 2021/src/bin/p01.rs
+++ 2021/src/bin/p01.rs
@@ -2,24 +2,17 @@ use anyhow::Result;
 use std::io::{self, BufRead};
 
 fn main() -> Result<()> {
-
     let mut depths: Vec<u64> = vec![];
 
     for line in io::stdin().lock().lines() {
         depths.push(line?.parse()?);
     }
 
-    let p1 = depths
-        .windows(2)
-        .filter(|w| w[1] > w[0])
-        .count();
+    let p1 = depths.windows(2).filter(|w| w[1] > w[0]).count();
 
     println!("Part 1: {}", p1);
 
-    let p2 = depths
-        .windows(4)
-        .filter(|w| w[3] > w[0])
-        .count();
+    let p2 = depths.windows(4).filter(|w| w[3] > w[0]).count();
 
     println!("Part 2: {}", p2);
 
