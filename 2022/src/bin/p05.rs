46bfd5a829873e94030be3c8462671f02f2391a8
Mike Sampson <mike@sambodata.com>
2022-12-05 21:45:39 +1100

    2022 Day 5

---
+++ 2022/src/bin/p05.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use std::io::{self, BufRead};
+
+fn main() -> Result<()> {
+    let mut stacks: Vec<Vec<char>> = vec![vec![]; 9];
+    let mut moves: Vec<(usize, usize, usize)> = vec![];
+
+    for line in io::stdin().lock().lines() {
+        let line = line?;
+
+        if line.contains('[') {
+            // process stack
+            for (i, c) in line.chars().enumerate().filter(|&(_, c)| c.is_uppercase()) {
+                stacks[(i - 1) / 4].insert(0, c); // insert is slow but stacks are small
+            }
+        } else if line.starts_with("move") {
+            // process move
+            let el: Vec<_> = line.split(' ').collect();
+            moves.push((el[1].parse()?, el[3].parse()?, el[5].parse()?));
+        }
+    }
+
+    println!("Part 1: {}", process(&mut stacks.clone(), &moves, false));
+
+    println!("Part 2: {}", process(&mut stacks, &moves, true));
+
+    Ok(())
+}
+
+fn process(stacks: &mut Vec<Vec<char>>, moves: &[(usize, usize, usize)], part2: bool) -> String {
+    for &(m, f, t) in moves {
+        let mut temp: Vec<_> = (0..m).filter_map(|_| stacks[f - 1].pop()).collect();
+
+        if part2 {
+            temp.reverse();
+        }
+
+        for c in temp {
+            stacks[t - 1].push(c);
+        }
+    }
+
+    stacks.iter().filter_map(|s| s.last()).collect()
+}
