5006d2198a90ba63b49d0d4170228ff59b9852b6
Mike Sampson <mfs@sda.io>
2024-11-03 17:47:11 +1100

    Prep for 2024.

---
+++ 2024/scripts/readme.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cargo build --release
+
+echo "# Advent of Code 2024" > README.md
+echo >> README.md
+
+echo "## Benchmarks" >> README.md
+echo >> README.md
+hyperfine -w 3 target/release/p?? --export-markdown hf.md
+cat hf.md >> README.md
+rm hf.md
+
+echo "## Lines of Code" >> README.md
+echo >> README.md
+scc --by-file -i rs -f csv --exclude-file template.rs --exclude-file csv2md.rs | cargo run --release --bin csv2md >> README.md
+echo >> README.md
+
+
---
+++ 2024/src/bin/csv2md.rs
@@ -0,0 +1,18 @@
+use std::io::{self, BufRead};
+use anyhow::Result;
+
+fn main() -> Result<()> {
+	let mut rows: Vec<Vec<String>> = vec![];
+
+    for line in io::stdin().lock().lines() {
+        rows.push(line?.split(',').map(String::from).collect());
+    }
+
+	println!("| {} |", rows[0].join(" | "));
+	println!("| :--- | :--- | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: |");
+	for r in &rows[1..] {
+		println!("| {} |", r.join(" | "));
+	}
+
+    Ok(())
+}
