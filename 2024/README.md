bdb3ff4626c97dad679a73eb505ad6761fc12171
Mike Sampson <mfs@sda.io>
2024-12-01 17:01:11 +1100

    Day 1.

--- 2024/README.md
+++ 2024/README.md
@@ -1,15 +1,8 @@
 # Advent of Code 2024
 
-## Benchmarks
-
-| Command | Mean [ms] | Min [ms] | Max [ms] | Relative |
-|:---|---:|---:|---:|---:|
-| `target/release/p01` | 1.2 ± 0.4 | 0.0 | 1.9 | 1.00 |
-| `target/release/p02` | 1.3 ± 0.3 | 0.0 | 2.1 | 1.05 ± 0.39 |
 ## Lines of Code
 
 | Language | Provider | Filename | Lines | Code | Comments | Blanks | Complexity | Bytes | ULOC |
 | :--- | :--- | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: |
-| Rust | src/bin/p01.rs | p01.rs | 57 | 44 | 0 | 13 | 5 | 1172 | 0 |
-| Rust | src/bin/p02.rs | p02.rs | 85 | 61 | 1 | 23 | 11 | 1888 | 0 |
+| Rust | src/bin/p01.rs | p01.rs | 39 | 28 | 0 | 11 | 2 | 805 | 0 |
 
---
+++ 2024/src/bin/p01.rs
@@ -0,0 +1,39 @@
+use std::iter::zip;
+use std::io::{self, BufRead};
+use std::collections::HashMap;
+use std::str::FromStr;
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+	let mut left = vec![];
+	let mut right = vec![];
+
+    for line in io::stdin().lock().lines() {
+        let x: Vec<i64> = line?
+            .split_whitespace()
+            .map(i64::from_str)
+            .collect::<Result<_, _>>()?;
+		left.push(x[0]);
+		right.push(x[1]);
+    }
+
+	left.sort_unstable();
+	right.sort_unstable();
+
+	let part1: i64 = zip(&left, &right).map(|(l, r)| (r - l).abs()).sum();
+
+	println!("Part 1: {}", part1);
+
+	let mut counts = HashMap::new();
+
+	for r in &right {
+		*counts.entry(r).or_insert(0) += 1;
+	}
+
+	let part2: i64 = left.iter().map(|l| l * counts.get(l).unwrap_or(&0)).sum();
+
+	println!("Part 2: {}", part2);
+
+    Ok(())
+}
