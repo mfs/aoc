a7ae1b753c36c51b6f4ed8e7d925e6e5f113b622
Mike Sampson <mfs@sda.io>
2024-12-02 22:44:43 +1100

    Fix benchmarking.

--- 2024/scripts/readme.sh
+++ 2024/scripts/readme.sh
@@ -7,7 +7,12 @@ echo >> README.md
 
 echo "## Benchmarks" >> README.md
 echo >> README.md
-hyperfine -w 3 "target/release/p01 < input/p01.txt" --export-markdown hf.md
+
+TARGETS=$(basename -a target/release/p??)
+
+DAYS=${TARGETS//$'\n'/,}
+
+hyperfine -w 3 -L day ${DAYS} "target/release/{day} < input/{day}.txt" --export-markdown hf.md
 cat hf.md >> README.md
 rm hf.md
 echo >> README.md
@@ -16,5 +21,3 @@ echo "## Lines of Code" >> README.md
 echo >> README.md
 scc --by-file -i rs -f csv --exclude-file template.rs --exclude-file csv2md.rs | cargo run --release --bin csv2md >> README.md
 echo >> README.md
-
-
