32866729165b91a6b74e211b5a236e27f50d042c
Mike Sampson <mfs@sda.io>
2024-12-05 00:55:28 +1100

    Day 4.

--- 2024/README.md
+++ 2024/README.md
@@ -4,9 +4,10 @@
 
 | Command | Mean [ms] | Min [ms] | Max [ms] | Relative |
 |:---|---:|---:|---:|---:|
-| `target/release/p01 < input/p01.txt` | 2.1 ± 0.7 | 0.0 | 3.0 | 1.00 |
-| `target/release/p02 < input/p02.txt` | 2.9 ± 0.8 | 0.0 | 4.1 | 1.41 ± 0.62 |
-| `target/release/p03 < input/p03.txt` | 3.6 ± 0.8 | 0.0 | 5.0 | 1.78 ± 0.74 |
+| `target/release/p01 < input/p01.txt` | 3.4 ± 0.4 | 0.5 | 4.1 | 1.00 |
+| `target/release/p02 < input/p02.txt` | 3.7 ± 1.2 | 0.7 | 5.0 | 1.10 ± 0.37 |
+| `target/release/p03 < input/p03.txt` | 4.4 ± 1.3 | 0.9 | 5.7 | 1.30 ± 0.42 |
+| `target/release/p04 < input/p04.txt` | 4.3 ± 1.4 | 0.9 | 6.6 | 1.28 ± 0.43 |
 
 ## Lines of Code
 
@@ -15,4 +16,5 @@
 | Rust | src/bin/p01.rs | p01.rs | 39 | 28 | 0 | 11 | 2 | 856 | 0 |
 | Rust | src/bin/p02.rs | p02.rs | 50 | 35 | 2 | 13 | 5 | 1189 | 0 |
 | Rust | src/bin/p03.rs | p03.rs | 34 | 26 | 0 | 8 | 2 | 883 | 0 |
+| Rust | src/bin/p04.rs | p04.rs | 92 | 62 | 6 | 24 | 21 | 2139 | 0 |
 
---
+++ 2024/src/bin/p04.rs
@@ -0,0 +1,92 @@
+use std::io::{self, BufRead};
+
+use anyhow::Result;
+
+type Grid = Vec<Vec<char>>;
+
+fn main() -> Result<()> {
+    let mut grid = Grid::new();
+
+    for line in io::stdin().lock().lines() {
+        grid.push(line?.chars().collect());
+    }
+
+    let part1 = xmas(&grid);
+
+    println!("Part 1: {}", part1);
+
+    let part2 = cross(&grid);
+
+    println!("Part 2: {}", part2);
+
+    Ok(())
+}
+
+fn cross(g: &Grid) -> u32 {
+    let w = g[0].len();
+    let h = g.len();
+
+    let mut count = 0;
+
+    for y in 1..h-1 {
+        for x in 1..w-1 {
+            // if not a continue
+            if g[y][x] != 'A' {
+                continue;
+            }
+
+            // a b
+            //  x
+            // c d
+
+            let (a, b, c, d) = (g[y-1][x-1], g[y-1][x+1], g[y+1][x-1], g[y+1][x+1]);
+
+            match (a, b, c, d) {
+                ('M', 'M', 'S', 'S') => count += 1,
+                ('S', 'M', 'S', 'M') => count += 1,
+                ('M', 'S', 'M', 'S') => count += 1,
+                ('S', 'S', 'M', 'M') => count += 1,
+                _ => {},
+            }
+        }
+    }
+
+    count
+}
+
+fn xmas(g: &Grid) -> u32 {
+    let w = g[0].len();
+    let h = g.len();
+
+    let mut count = 0;
+
+    const DIRECTIONS: [(i32, i32); 8] = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)];
+
+    let grid = |y: i32, x: i32| g[y as usize][x as usize];
+
+    for y in 0..(h as i32) {
+        for x in 0..(w as i32) {
+            for dir in DIRECTIONS {
+                // check end point is in bounds
+                let end = (x + 3 * dir.0, y + 3 * dir.1);
+                if end.0 < 0 || end.0 >= w as i32 || end.1 < 0 || end.1 >= h as i32 {
+                    continue;
+                }
+
+                // check for XMAS
+                let (a, b, c, d) = (
+                    grid(y, x),
+                    grid(y + dir.1, x + dir.0),
+                    grid(y + 2 * dir.1, x + 2 * dir.0),
+                    grid(y + 3 * dir.1, x + 3 * dir.0),
+                );
+
+                if a == 'X' && b == 'M' && c == 'A' && d == 'S' {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    count
+}
