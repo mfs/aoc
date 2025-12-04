fce992d044f389dd3a6c1cf678cad33d4ddf4b5d
Mike Sampson <mfs@sda.io>
2025-12-05 01:13:56 +1100

    Day 4.

--- 2025/README.md
+++ 2025/README.md
@@ -32,15 +32,17 @@ I tend to rely on minimal external libraries though do have a few standard ones 
 
 | Command | Mean [ms] | Min [ms] | Max [ms] |
 |:---|---:|---:|---:|
-| `target/release/p01 < input/p01.txt` | 7.2 ± 3.0 | 2.9 | 11.0 |
-| `target/release/p02 < input/p02.txt` | 55.3 ± 6.1 | 46.5 | 69.3 |
-| `target/release/p03 < input/p03.txt` | 0.5 ± 0.1 | 0.4 | 0.6 |
+| `target/release/p01 < input/p01.txt` | 10.0 ± 1.9 | 5.1 | 11.1 |
+| `target/release/p02 < input/p02.txt` | 18.2 ± 3.4 | 15.0 | 26.3 |
+| `target/release/p03 < input/p03.txt` | 1.0 ± 0.2 | 0.7 | 1.3 |
+| `target/release/p04 < input/p04.txt` | 16.9 ± 0.8 | 16.4 | 19.0 |
 
 ## Lines of Code
 
 | Language | Provider | Filename | Lines | Code | Comments | Blanks | Complexity | Bytes | ULOC |
 | :--- | :--- | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: |
 | Rust | src/bin/p01.rs | p01.rs | 50 | 38 | 0 | 12 | 8 | 1100 | 0 |
-| Rust | src/bin/p02.rs | p02.rs | 77 | 57 | 0 | 20 | 11 | 1669 | 0 |
+| Rust | src/bin/p02.rs | p02.rs | 79 | 58 | 0 | 21 | 11 | 1758 | 0 |
 | Rust | src/bin/p03.rs | p03.rs | 59 | 43 | 1 | 15 | 5 | 1387 | 0 |
+| Rust | src/bin/p04.rs | p04.rs | 74 | 55 | 0 | 19 | 12 | 1587 | 0 |
 
---
+++ 2025/src/bin/p04.rs
@@ -0,0 +1,74 @@
+use std::io::{self, BufRead};
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let mut  grid = parse()?;
+
+    let mut rolls = remove_rolls(&mut grid);
+
+    println!("Part 1: {}", rolls);
+
+    rolls += (0..)
+        .map(|_| remove_rolls(&mut grid))
+        .take_while(|x| *x != 0)
+        .sum::<usize>();
+
+    println!("Part 2: {}", rolls);
+
+    Ok(())
+}
+
+fn remove_rolls(grid: &mut Vec<Vec<char>>) -> usize {
+    let w = grid[0].len();
+    let h = grid.len();
+
+    let mut remove = vec![];
+
+    for y in 0..h {
+        for x in 0..w {
+            if grid[y][x] != '@' {
+                continue;
+            }
+
+            let count = neighbours(x, y, w, h)
+                .iter()
+                .filter(|(x, y)| grid[*y][*x] == '@')
+                .count();
+
+            if count < 4 {
+                remove.push((x, y));
+            }
+        }
+    }
+
+    for &(x, y) in &remove {
+        grid[y][x] = '.';
+    }
+
+    remove.len()
+}
+
+const NEIGHBOURS: [(i32, i32); 8] = [
+    (-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1),
+];
+
+fn neighbours(x: usize, y: usize, w: usize, h: usize) -> Vec<(usize, usize)> {
+    NEIGHBOURS
+        .iter()
+        .map(|(dx, dy)| (x as i32 + dx, y as i32 + dy))
+        .filter(|&(nx, ny)| nx >= 0 && nx < w as i32 && ny >= 0 && ny < h as i32)
+        .map(|(nx, ny)| (nx as usize, ny as usize))
+        .collect()
+}
+
+fn parse() -> Result<Vec<Vec<char>>> {
+    let mut grid = vec![];
+
+    for line in io::stdin().lock().lines() {
+        let line = line?;
+        grid.push(line.chars().collect());
+    }
+
+    Ok(grid)
+}
